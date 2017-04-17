92e06f33b13f506d780996a017e7f7155d5f8338
njgheorghita <njgheorghita@email.wm.edu>
2017-04-17 08:36:34 -0600

    Top 10 read links displayed on index implemented

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,10 +1,9 @@
 class LinksController < ApplicationController
   def index
-    @links = Link.all
-    # Link.select("links.*, COUNT(reads.id) read_count")
-    #     .joins(:reads)
-    #     .where() # read.created_at is in past 24 hours
-    #     .group("links.id")
-    #     .order("read_count DESC")
+    @links = Link.select("links.*, COUNT(reads.id) read_count")
+                  .joins(:reads)
+                  .group("links.id")
+                  .order("read_count DESC")
+                  .limit(10)
   end
 end
