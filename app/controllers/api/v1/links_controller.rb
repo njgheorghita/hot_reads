4ad59fd04e514b125ef05d93126310a2ffc69e3d
njgheorghita <njgheorghita@email.wm.edu>
2017-04-17 09:12:28 -0600

    Hot links implemented

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -13,6 +13,27 @@ class Api::V1::LinksController < ApplicationController
   end
 
   def update_tags
-    puts "yo"
+    Link.all.each do |link|
+      link.tag = nil
+      link.save
+    end
+    
+    Link.select("links.*, COUNT(reads.id) read_count")
+      .joins(:reads)
+      .group("links.id")
+      .order("read_count DESC")
+      .limit(10).each do |link|
+      link.tag = "top link"
+      link.save
+    end
+
+    Link.select("links.*, COUNT(reads.id) read_count")
+      .joins(:reads)
+      .group("links.id")
+      .order("read_count DESC")
+      .limit(1).each do |link|
+      link.tag = "hot"
+      link.save
+    end
   end
 end
--- spec/requests/features/visitor_can_view_links_spec.rb
+++ spec/requests/features/visitor_can_view_links_spec.rb
@@ -1,49 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe "Visitor", type: :feature do 
+RSpec.describe "Visitor", type: [:feature, :request] do 
   context "can visit homepage" do 
 
     before :each do 
-      one = Link.create(url: "www.one.com")
-      two = Link.create(url: "www.two.com")
-      three = Link.create(url: "www.three.com")
-      four = Link.create(url: "www.four.com")
-      five = Link.create(url: "www.five.com")
-      six = Link.create(url: "www.six.com")
-      seven = Link.create(url: "www.seven.com")
-      eight = Link.create(url: "www.eight.com")
-      nine = Link.create(url: "www.nine.com")
-      ten = Link.create(url: "www.ten.com")
-      eleven = Link.create(url: "www.eleven.com")
-
-      Read.create(link_id: one.id)
-      Read.create(link_id: one.id)
-      Read.create(link_id: two.id)
-      Read.create(link_id: two.id)
-      Read.create(link_id: three.id)
-      Read.create(link_id: three.id)
-      Read.create(link_id: four.id)
-      Read.create(link_id: four.id)
-      Read.create(link_id: five.id)
-      Read.create(link_id: five.id)
-      Read.create(link_id: six.id)
-      Read.create(link_id: six.id)
-      Read.create(link_id: seven.id)
-      Read.create(link_id: seven.id)
-      Read.create(link_id: eight.id)
-      Read.create(link_id: eight.id)
-      Read.create(link_id: nine.id)
-      Read.create(link_id: nine.id)
-      Read.create(link_id: ten.id)
-      Read.create(link_id: ten.id)
-      Read.create(link_id: ten.id)
-      Read.create(link_id: eleven.id)
+      post api_v1_links_path, params: { url:"www.one.com" }
+      post api_v1_links_path, params: { url:"www.one.com" }
+      post api_v1_links_path, params: { url:"www.two.com" }
+      post api_v1_links_path, params: { url:"www.two.com" }
+      post api_v1_links_path, params: { url:"www.three.com" }
+      post api_v1_links_path, params: { url:"www.three.com" }
+      post api_v1_links_path, params: { url:"www.four.com" }
+      post api_v1_links_path, params: { url:"www.four.com" }
+      post api_v1_links_path, params: { url:"www.five.com" }
+      post api_v1_links_path, params: { url:"www.five.com" }
+      post api_v1_links_path, params: { url:"www.six.com" }
+      post api_v1_links_path, params: { url:"www.six.com" }
+      post api_v1_links_path, params: { url:"www.seven.com" }
+      post api_v1_links_path, params: { url:"www.seven.com" }
+      post api_v1_links_path, params: { url:"www.eight.com" }
+      post api_v1_links_path, params: { url:"www.eight.com" }
+      post api_v1_links_path, params: { url:"www.nine.com" }
+      post api_v1_links_path, params: { url:"www.nine.com" }
+      post api_v1_links_path, params: { url:"www.ten.com" }
+      post api_v1_links_path, params: { url:"www.ten.com" }
+      post api_v1_links_path, params: { url:"www.ten.com" }
+      post api_v1_links_path, params: { url:"www.eleven.com" }
     end
 
     it "and see the top ten most read urls and corresponding tags" do 
       visit root_path
-      expect(page).to have_content("www.one.com reads: 2 tag: hot")
-      expect(page).to have_content("www.ten.com reads: 3 tag: top link")
+      expect(page).to have_content("www.one.com reads: 2 tag: top link")
+      expect(page).to have_content("www.ten.com reads: 3 tag: hot")
       expect(page).not_to have_content("www.eleven.com")
     end
   end
